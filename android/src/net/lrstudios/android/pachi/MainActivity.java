5c3a9d9b31ad24ea17edbac98495809be9cd7db4
Emmanuel Mathis <emmanuel@lr-studios.net>
2013-05-15 15:56:09 +0200

    Renamed main activity from PachiNewGameActivity to MainActivity. Updated to version 3 (1.1).

--- android/src/net/lrstudios/android/pachi/MainActivity.java
+++ android/src/net/lrstudios/android/pachi/MainActivity.java
@@ -3,7 +3,7 @@ package net.lrstudios.android.pachi;
 import lrstudios.games.ego.lib.ui.NewGameActivity;
 
 
-public class PachiNewGameActivity extends NewGameActivity {
+public class MainActivity extends NewGameActivity {
     @Override
     protected Class<?> getBotClass() {
         return PachiEngine.class;
